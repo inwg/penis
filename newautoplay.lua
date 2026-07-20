ba9b3e19297a21a56d58d1d488a9621308d85433
inwg <109669438+inwg@users.noreply.github.com>
2026-07-20 22:04:10 +0800

    Update newautoplay.lua

--- newautoplay.lua
+++ newautoplay.lua
@@ -378,10 +378,13 @@ end)
 
 local Lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/neaxusxgod-png/INS-ui/main/uilib.min.lua"))() or INSui
 local win = Lib:CreateWindow({
-    title    = "Gakuran",
+    title    = "幹你娘",
     size     = Vector2.new(400, 400),
     menuKey  = "f1",
+    logo     = "https://raw.githubusercontent.com/inwg/penis/refs/heads/main/%E4%B8%8B%E8%BC%89%20(7).jpg",
 })
+
+win:AddSettingsTab("cog")
 
 local mainTab = win:Tab("Main")
 local settingsSec = mainTab:Section("Settings", "Full")
